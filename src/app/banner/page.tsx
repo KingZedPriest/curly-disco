d3f561e4021f88f9be9c3113685cc6f15fb91912
KingZedPriest <Charleschukwuemeka47@gmail.com>
2023-09-05 18:27:26 +0100

    Fix: Finalise things

--- src/app/banner/page.tsx
+++ src/app/banner/page.tsx
@@ -9,25 +9,25 @@ import Link from "next/link";
 export default function Banner() {
   const [name, setName] = useState<any>();
   const [image, setImage] = useState<any>();
-  const [isVisible, setIsVisible] = useState<boolean>(false)
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-const handleDownload = () => {
-  const divToCapture = document.getElementById('content');
-  if (!divToCapture) return;
+  const handleDownload = () => {
+    const divToCapture = document.getElementById("content");
+    if (!divToCapture) return;
 
-  html2canvas(divToCapture).then((canvas) => {
-    const imageDataURL = canvas.toDataURL('image/png'); 
+    html2canvas(divToCapture).then((canvas) => {
+      const imageDataURL = canvas.toDataURL("image/png");
 
-    const a = document.createElement('a');
-    a.href = imageDataURL;
-    a.download = `${name}-fastfood.png`;
-    a.click();
-  });
-};
+      const a = document.createElement("a");
+      a.href = imageDataURL;
+      a.download = `${name}-fastfood.png`;
+      a.click();
+    });
+  };
 
-const toggleVisibility = () => {
-  setIsVisible(!isVisible);
-};
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
 
   useEffect(() => {
     setName(localStorage.getItem("name"));
@@ -35,70 +35,75 @@ const toggleVisibility = () => {
   }, []);
 
   return (
-    <main className="lg:w-5/6 mx-auto lg:pt-16 pt-10">
- {isVisible && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-slate-400">
-          <div className="bg-black p-8 rounded shadow-md">
-            <p className="text-lg mb-4">This is your popup content.</p>
-            <Link href="/"><button
-              onClick={handleDownload}
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-            >
-              Download Image
-            </button></Link>
+    <main className="mx-auto pt-10 lg:w-5/6 lg:pt-16">
+      {isVisible && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-400">
+          <div className="w-80 rounded bg-black py-8 text-center shadow-md">
+            <p className="mb-4 text-xs">
+              Want a Professional Website?
+              <br /> Contact <span className="text-sm font-semibold text-[#AB2B2B]"> Priest 09067789223</span>
+              .
+            </p>
+            <Link href="/">
+              <button
+                onClick={handleDownload}
+                className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+              >
+                Download Image
+              </button>
+            </Link>
           </div>
         </div>
-         )}
+      )}
       <Link
         href="/"
-        className="flex items-center gap-2 mb-5 ml-5 w-fit border border-purple-700 px-4 py-2 rounded-md hover:bg-purple-700 duration-500"
+        className="mb-5 ml-5 flex w-fit items-center gap-2 rounded-md border border-purple-700 px-4 py-2 duration-500 hover:bg-purple-700"
       >
         <BiArrowBack />
         Back
       </Link>
-      <div className={`${isVisible ? 'hidden' : 'flex'} flex mx-auto justify-center w-[500px] h-[500px]`}>
+      <div
+        className={`${
+          isVisible ? "hidden" : "flex"
+        } mx-auto flex h-[500px] w-[500px] justify-center`}
+      >
         <div className="relative">
-          <Image
-            src={bannerpic}
-            alt="Picture of Church Dp banner"
-            priority
-          />
+          <Image src={bannerpic} alt="Picture of Church Dp banner" priority />
 
-          <div className="absolute top-[60px] right-[110px] border-2 border-[#AB2B2B] rounded-[50%] p-1">
-              <Image
-                src={image}
-                alt="User's Photo"
-                width="185"
-                height="50"
-                className="h-[70px] w-[70px] rounded-[50%]"
-                priority
-              />
+          <div className="absolute right-[110px] top-[60px] rounded-[50%] border-2 border-[#AB2B2B] p-1">
+            <Image
+              src={image}
+              alt="User's Photo"
+              width="185"
+              height="50"
+              className="h-[70px] w-[70px] rounded-[50%]"
+              priority
+            />
           </div>
         </div>
       </div>
-      <div className={`${isVisible ? 'flex' : 'hidden'} justify-center`}>
-        <div id="content" className="min-w-[1080px] relative h-[1080px] overflow-x-auto">
-          <Image
-            src={bannerpic}
-            alt="Picture of Church Dp banner"
-            priority
-          />
+      <div className={`${isVisible ? "flex" : "hidden"} justify-center`}>
+        <div
+          id="content"
+          className="relative h-[1080px] min-w-[1080px] overflow-x-auto"
+        >
+          <Image src={bannerpic} alt="Picture of Church Dp banner" priority />
 
-          <div className="absolute top-[170px] right-[260px] border-2 border-[#AB2B2B] rounded-[50%] p-1">
-              <Image
-                src={image}
-                alt="User's Photo"
-                width="185"
-                height="50"
-                className="h-[120px] w-[120px] rounded-[50%]"
-                priority
-              />
+          <div className="absolute right-[260px] top-[170px] rounded-[50%] border-2 border-[#AB2B2B] p-1">
+            <Image
+              src={image}
+              alt="User's Photo"
+              width="185"
+              height="50"
+              className="h-[120px] w-[120px] rounded-[50%]"
+              priority
+            />
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-8">
+      <div className="mt-8 flex justify-center">
         <button
-          className="bg-violet-600 border-2 border-violet-600 duration-500 font-semibold rounded-lg px-3 py-2 text-white hover:bg-white hover:text-violet-600"
+          className="rounded-lg border-2 border-violet-600 bg-violet-600 px-3 py-2 font-semibold text-white duration-500 hover:bg-white hover:text-violet-600"
           onClick={toggleVisibility}
         >
           Continue
